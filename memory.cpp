e22cc4aab62cb64d73155f48fbbaf08a2799ecc8
Endpoints1337 <115175031+Endpoints1337@users.noreply.github.com>
2023-03-02 10:27:11 -0600

    Update memory.cpp

--- memory.cpp
+++ memory.cpp
@@ -1,1 +1,19 @@
+#include <windows.h>
+#Include <iostream>
 
+
+using namespace std;
+
+void memory() {
+system("echo %USER% - User-Identifier| (%RANDOM%)");
+  
+  
+  
+}
+
+
+
+
+int main() {
+   std::cout << "Their";
+}
