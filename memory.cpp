b65003c5a4d6d358c8f1e3c03d37eedcdfb7e0d9
Endpoints1337 <115175031+Endpoints1337@users.noreply.github.com>
2023-03-06 09:02:20 -0600

    Update memory.cpp

--- memory.cpp
+++ memory.cpp
@@ -1,19 +1,41 @@
-#include <windows.h>
-#Include <iostream>
+#include <Windows.h>
+#include <iostream>
 
+int main()
+{
+    DWORD pid = 1234; // replace with actual process ID
+    HANDLE processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
+    if (!processHandle)
+    {
+        std::cerr << "Error: Unable to open process with ID " << pid << std::endl;
+        return 1;
+    }
 
-using namespace std;
+    // Read memory
+    int address = 0x12345678; // replace with actual address to read
+    int value = 0;
+    SIZE_T bytesRead = 0;
+    BOOL success = ReadProcessMemory(processHandle, (LPVOID)address, &value, sizeof(value), &bytesRead);
+    if (!success || bytesRead != sizeof(value))
+    {
+        std::cerr << "Error: Unable to read memory at address " << std::hex << address << std::endl;
+        CloseHandle(processHandle);
+        return 1;
+    }
+    std::cout << "Value at address " << std::hex << address << ": " << value << std::endl;
 
-void memory() {
-system("echo %USER% - User-Identifier| (%RANDOM%)");
-  
-  
-  
-}
+    // Write memory
+    int newValue = 12345; // replace with actual value to write
+    SIZE_T bytesWritten = 0;
+    success = WriteProcessMemory(processHandle, (LPVOID)address, &newValue, sizeof(newValue), &bytesWritten);
+    if (!success || bytesWritten != sizeof(newValue))
+    {
+        std::cerr << "Error: Unable to write memory at address " << std::hex << address << std::endl;
+        CloseHandle(processHandle);
+        return 1;
+    }
+    std::cout << "New value at address " << std::hex << address << ": " << newValue << std::endl;
 
-
-
-
-int main() {
-   std::cout << "Their";
+    CloseHandle(processHandle);
+    return 0;
 }
